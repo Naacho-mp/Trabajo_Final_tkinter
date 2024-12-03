6e249d7edf0e21b8a483afef3867bdb300834e70
Nacho <nachomatamala@gmail.com>
2024-12-02 21:02:33 -0300

    filtra por jpg-png

--- nombre_archivos.py
+++ nombre_archivos.py
@@ -2,6 +2,13 @@ from pathlib import Path
 
 ruta = Path('C:/Users/Ignacio/Desktop/plantas')
 
-archivos = [archivo.name for archivo in ruta.iterdir() if archivo.is_file()]
+archivos = [archivo.name for archivo in ruta.iterdir()
+            if archivo.is_file()]
 
-print(archivos)
+solo_img = []
+
+for i in archivos:
+    if i.endswith(('.jpg', '.png')):
+        solo_img.append(i)
+
+print(solo_img)
